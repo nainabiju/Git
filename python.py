598ffbf663c1e0b6bcd89249a6a099ad59d39406
nainabiju <jagnes007@gmail.com>
2024-10-09 09:44:10 +0530

    My python introduction

---
+++ python.py
@@ -0,0 +1,13 @@
+def self_introduction(name, age, hobby, profession):
+    print("Hello! My name is", name)
+    print("I am", age, "years old.")
+    print("I enjoy", hobby + ".")
+    print("I work as a", profession + ".")
+
+# Customize your details here
+my_name = "Alice"
+my_age = 30
+my_hobby = "reading books"
+my_profession = "software developer"
+
+self_introduction(my_name, my_age, my_hobby, my_profession)
